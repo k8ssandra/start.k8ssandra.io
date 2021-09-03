55c46fd5ee28ee03635fbea2442bbce3a0cdbe53
Conor Driscoll <conor.driscoll@teamcolab.com>
2021-09-03 17:17:54 -0400

    first draft - model, state store, mutations

--- src/store/index.js
+++ src/store/index.js
@@ -10,6 +10,95 @@ export default new Vuex.Store({
       description: "",
       version: "4.0.0",
       authentication: "true",
+      cluster_size: 1,
+      datacenters: [
+        {
+          name: "",
+          racks: [],
+        },
+      ],
+      cpu_cores: {
+        amt: 1,
+        unit: " ",
+      },
+      ram_cores: {
+        amt: 1,
+        unit: "MB",
+      },
+      storage: {
+        class: "",
+        amt: 1,
+        unit: "MB",
+      },
+      heap: {
+        amt: 0.25,
+        suffix: "MB",
+      },
+      additional_seeds: [],
+      stargate: {
+        active: false,
+        size: 1,
+        cpu: 1,
+        unit: "",
+        heap_in_mb: 0,
+      },
+      reaper: {
+        active: false,
+      },
+      medusa: {
+        active: false,
+        provider: [
+          {
+            name: "S3",
+            config: {
+              region: "",
+              bucket_name: "",
+              secret: "",
+            },
+          },
+          {
+            name: "S3 Compatible",
+            config: {
+              region: "",
+              host: "",
+              port: 0,
+              secure: false,
+              bucketname: "",
+              secret: "",
+            },
+          },
+          {
+            name: "Google Cloud Storage",
+            config: {
+              bucketname: "",
+              secret: ""
+            }
+          },
+          {
+            name: "Azure Blob Configuration",
+            config: {
+              bucketname: "",
+              secret: ""
+            }
+          },
+          {
+            name: "Local Configuration",
+            config: {
+              storage_class: "",
+              size: 1,
+              unit: "MB"
+            }
+          },
+        ],
+      },
+      monitoring: {
+        active: false,
+        config: {
+          kube_prometheus_stack: false,
+          service_monitors: false,
+          dashboards: false
+        }
+      }
     },
   },
   mutations: {
@@ -24,6 +113,36 @@ export default new Vuex.Store({
     },
     updateAuthentication(state, txt) {
       state.k8_config.authentication = txt;
+    },
+    updateClusterSize(state, txt) {
+      state.k8_config.cluster_size = txt;
+    },
+    updateDataCenterName(state, txt) {
+      state.k8_config.datacenters[0].name = txt;
+    },
+    updateCpuCoresAmount(state, txt) {
+      state.k8_config.cpu_cores.amt = txt;
+    },
+    updateCpuCoresUnit(state, txt) {
+      state.k8_config.cpu_cores.unit = txt;
+    },
+    updateRamCoresAmount(state, txt) {
+      state.k8_config.ram_cores.amt = txt;
+    },
+    updateRamCoresUnit(state, txt) {
+      state.k8_config.ram_cores.unit = txt;
+    },
+    updateStorageClass(state, txt) {
+      state.k8_config.storage.class = txt;
+    },
+    updateStorageAmount(state, txt) {
+      state.k8_config.storage.amt = txt;
+    },
+    updateStorageUnit(state, txt) {
+      state.k8_config.storage.unit = txt;
+    },
+    updateStargateEnabled(state, txt) {
+      state.k8_config.stargate.active = txt;
     },
   },
   actions: {},
