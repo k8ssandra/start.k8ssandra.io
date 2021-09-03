ea71b3481b5e7f28d7b852b6e2c307a80f88c736
Conor Driscoll <conor.driscoll@teamcolab.com>
2021-09-03 17:20:07 -0400

    stargate - wip

---
+++ src/components/Stargate.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="stargate">
+    <h2>Stargate</h2>
+    <div>
+        Enabled:
+        <input type="checkbox" id="checkbox" v-model="enabled">
+        <label for="checkbox"> {{ enabled }}</label>
+    </div>
+    <div v-if="enabled" class="settings">
+        Stargate Settings
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Stargate",
+  computed: {
+    enabled: {
+      get() {
+        return this.$store.state.k8_config.stargate.active;
+      },
+      set(value) {
+        this.$store.commit("updateStargateEnabled", value);
+      },
+    },
+  }
+};
+</script>
