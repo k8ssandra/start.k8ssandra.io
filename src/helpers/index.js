9537ee60be577c4357c46664ba5d65ee45365fbc
Conor Driscoll <conor.driscoll@teamcolab.com>
2021-09-08 12:42:37 -0400

    started querystring helperfunction

---
+++ src/helpers/index.js
@@ -0,0 +1,9 @@
+
+
+const fillform = () => {
+        this.$store.commit("updateName", this.$route.query.name);
+}
+
+export default {
+    fillform
+}
--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import helpers from './helpers'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  helpers,
   render: h => h(App)
 }).$mount('#app')
