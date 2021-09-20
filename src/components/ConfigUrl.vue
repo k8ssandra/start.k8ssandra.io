24c5c0c8cec1647f1ff74d8838b578ff877ca2d2
Conor Driscoll <conor.driscoll@teamcolab.com>
2021-09-20 14:53:50 -0400

    configurl

--- src/components/ConfigUrl.vue
+++ src/components/ConfigUrl.vue
@@ -1,12 +1,34 @@
 <template>
     <div>
         <h2>Share This URL</h2>
+        {{ base64endcoded }}
         </div>
         
 </template>
 
 <script>
 export default {
+  name: "ConfigUrl",
+  computed: {
+    base64endcoded() {
+    //   const path = this.$store.state;
+      
+      let buildPathConnect = {
+          name: this.$store.state.config.cassandra.clusterName,
+          description: this.$store.state.k8_config.description
+      };
+
+      let builtPath = window.location.origin + "?"
     
-}
+      for (let stone in buildPathConnect) {
+        let val = encodeURIComponent(buildPathConnect[stone]);
+         builtPath = builtPath + stone + "=" + val + "&";
+      }
+
+       
+
+      return builtPath;
+    },
+  },
+};
 </script>
