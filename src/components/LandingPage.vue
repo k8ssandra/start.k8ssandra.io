4a9138f28328ec1b9b2b7c3ebe2d80a6b0744dd9
Conor Driscoll <conor.driscoll@teamcolab.com>
2021-09-21 15:09:04 -0400

    set up templates system.

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,17 +1,36 @@
 <template>
    <div class="landing-page">
        <h1>Landing Page</h1>
-       <button @click.prevent="start">Start</button>
+        <div>
+            <button @click.prevent="start">Start</button>
+        </div>
+        
+        <h3> or Start with a template </h3>
+
+        <div class="templates">
+            <button @click.prevent="start(0)">Local Template</button>
+            <button @click.prevent="start(1)">Amazon AKS</button>            
+            <button @click.prevent="start(2)">Digital Ocean DOK</button>
+            <button @click.prevent="start(3)">Amazon EKS</button>            
+            <button @click.prevent="start(4)">Google GKE</button>
+        </div>
    </div> 
 </template>
 
 <script>
+import templates from "@/json/templates";
+
 export default {
-    methods: {
-        start() {
-            this.$store.commit('LandingPage')
-        }
-    }
+  methods: {
+    start(setup) {
+      this.$store.commit("LandingPage");
+      let fillValues = templates[setup];
+
+      for (let value in fillValues) {
+        this.$store.commit(value, fillValues[value]);
+      }
+    },
+  },
 };
 </script>
 
@@ -24,5 +43,12 @@ export default {
   right: 0;
   overflow: hidden;
   background-color: #eee;
+}
+.templates {
+    display:flex;
+    justify-content: center;
+    button {
+        margin: 5px;
+    }
 }
 </style>
