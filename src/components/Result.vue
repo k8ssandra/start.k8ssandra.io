06e1ab77990782fa1c582e7ab8fb9762a246de07
Conor Driscoll <conor.driscoll@teamcolab.com>
2021-09-24 10:59:02 -0400

    fixed sanitized local

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -68,12 +68,19 @@ export default {
       if (data.reaper.enabled !== true) {
         delete translatedData["reaper"]
       }
-      if (data.medusa.enabled !== true) {
-        delete translatedData["medusa"]
-      }
       if (data.medusa.storage === "local") {
         delete translatedData.medusa.bucketName
         delete translatedData.medusa.storageSecret
+      } 
+      if (data.medusa.storage !== "local") {
+        delete translatedData.medusa.storageClass
+        delete translatedData.medusa.size
+      }
+      if (data.medusa.storage !== "s3_compatible") {
+        delete translatedData.medusa.storage_properties
+      }
+      if (data.medusa.enabled !== true) {
+        delete translatedData["medusa"]
       }
       if (data.monitoring.prometheus.provision_service_monitors !== true) {
         delete translatedData.monitoring.prometheus
