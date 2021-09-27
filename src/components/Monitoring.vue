94bb8b501f7ad7cb8195346685a1e8de46645969
gtrost <gretel.trost@gmail.com>
2021-09-27 16:35:06 -0300

    Monitoring change checkbox to switch, basic styles

--- src/components/Monitoring.vue
+++ src/components/Monitoring.vue
@@ -1,23 +1,56 @@
 <template>
 <div class="module monitoring">
-  <h2>Monitoring</h2>
+  <h3>Monitoring</h3>
 
   <div>
-      Kube Prometheus Stack:
-      <input type="checkbox" id="checkbox" v-model="kube_prometheus">
-      <label for="checkbox"> {{ kube_prometheus }}</label>
+      Kube Prometheus Stack
+<!--      <input type="checkbox" id="checkbox" v-model="kube_prometheus">-->
+<!--      <label for="checkbox"> {{ kube_prometheus }}</label>-->
+      <v-switch
+          v-model="kube_prometheus"
+          inset
+      >
+        <template v-slot:prepend>
+          False
+        </template>
+        <template v-slot:append>
+          True
+        </template>
+      </v-switch>
   </div>
-      <div>
-      Service Monitors:
-      <input type="checkbox" id="checkbox" v-model="service_monitors">
-      <label for="checkbox"> {{ service_monitors }}</label>
+  <div>
+      Service Monitors
+<!--      <input type="checkbox" id="checkbox" v-model="service_monitors">-->
+<!--      <label for="checkbox"> {{ service_monitors }}</label>-->
+      <v-switch
+          v-model="service_monitors"
+          inset
+      >
+        <template v-slot:prepend>
+          False
+        </template>
+        <template v-slot:append>
+          True
+        </template>
+      </v-switch>
   </div>
-      <div>
-      Dashboards:
-      <input type="checkbox" id="checkbox" v-model="dashboards">
-      <label for="checkbox"> {{ dashboards }}</label>
-    </div>
+  <div>
+      Dashboards
+<!--      <input type="checkbox" id="checkbox" v-model="dashboards">-->
+<!--      <label for="checkbox"> {{ dashboards }}</label>-->
+      <v-switch
+          v-model="dashboards"
+          inset
+      >
+        <template v-slot:prepend>
+          False
+        </template>
+        <template v-slot:append>
+          True
+        </template>
+      </v-switch>
   </div>
+</div>
 </template>
 
 <script>
@@ -52,3 +85,19 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+  .monitoring > div {
+    margin-top: 30px;
+    display: flex;
+    align-items: center;
+    background: var(--color-grey-light);
+    padding: 20px;
+    font-size: 14px;
+    font-weight: 700;
+
+    .v-input {
+      margin-left: auto;
+    }
+  }
+</style>
