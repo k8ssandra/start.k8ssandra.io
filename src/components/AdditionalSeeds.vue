2edfa4e5059487c7e17ac8a77e6f420f84f95fb7
gtrost <gretel.trost@gmail.com>
2021-10-01 13:29:06 -0300

    Additional seeds styles

--- src/components/AdditionalSeeds.vue
+++ src/components/AdditionalSeeds.vue
@@ -4,8 +4,16 @@
             <ul class="seeds_list">
               <li class="seeds__seed" v-for="(seed, num) in seeds" :key="num">
                   <div class="seed__primary">
-                      {{seed}}
-                <button @click.prevent="removeSeed(num)">x</button>
+                    <v-text-field
+                        :value=seed
+                        readonly
+                    ></v-text-field>
+                <button @click.prevent="removeSeed(num)">
+                  <v-icon
+                  >
+                    fa-trash
+                  </v-icon>
+                </button>
                 </div>
                   </li>
             </ul>
@@ -15,7 +23,7 @@
                     placeholder="Seed name"
                     minlength=3
                 ></v-text-field>
-                <input type="submit"  value="add">
+                <input type="submit"  value="Add Seed">
               </form>    </div>
 </template>
 
@@ -53,6 +61,31 @@ export default {
 
 <style lang="scss" scoped>
 .seeds_list {
+  padding: 0;
   list-style: none;
+}
+
+.seed__primary {
+  display: flex;
+  margin-bottom: 10px;
+  align-items: center;
+
+  .v-text-field {
+    margin-right: 15px;
+  }
+
+  button {
+    margin-right: 5px;
+    margin-left: auto;
+  }
+}
+
+input[type="submit"] {
+  margin-top: 10px;
+}
+
+.v-icon.v-icon {
+  font-size: 16px;
+  color: var(--color-brand-light-blue);
 }
 </style>
